ead4f005effc4065b2db81c0ea237273fa8d34f3
AndreyGot <andreygot88@gmail.com>
2016-11-15 21:38:21 +0200

    UI countries TABS

--- src/Guide/CountrysBundle/Resources/public/js/guideApp.js
+++ src/Guide/CountrysBundle/Resources/public/js/guideApp.js
@@ -16,18 +16,24 @@ guideApp.controller('countryCtrl', function ($scope, $http) {
     $http({method: 'GET', url: '/country'}).then(
         function successCallback(response) {
             $scope.countries = response.data;
+
+            console.log($scope.countries);
+            $scope.currentActiveCountry = null;
+
         },
         function errorCallback(response) {
 
         }
     );
 
-    $http({method: 'GET', url: '/city'}).then(
-        function successCallback(response) {
-            $scope.cities = response.data;
-        },
-        function errorCallback(response) {
+    // $http({method: 'GET', url: '/city'}).then(
+    //     function successCallback(response) {
+    //         $scope.cities = response.data;
+    //     },
+    //     function errorCallback(response) {
 
-        }
-    );
+    //     }
+    // );
+
+                // if (country.id == 1) {country.class = 'active'};
 });
